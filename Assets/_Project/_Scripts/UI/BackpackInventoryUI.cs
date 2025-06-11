6116ed06d2b481cabcc082baa8337f9a578285ad
Merkatsuki <eatower2@gmail.com>
2025-06-11 16:08:01 -0400

    Updated BackpackInventoryUI to have a public RefreshUI

--- Assets/_Project/_Scripts/UI/BackpackInventoryUI.cs
+++ Assets/_Project/_Scripts/UI/BackpackInventoryUI.cs
@@ -73,7 +73,7 @@ namespace HairvestMoon.UI
             }
         }
 
-        private void RefreshUI()
+        public void RefreshUI()
         {
             for (int i = 0; i < BackpackInventorySystem.MaxSlots; i++)
             {
